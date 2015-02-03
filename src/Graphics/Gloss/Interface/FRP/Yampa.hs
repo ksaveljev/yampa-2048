22b67428534bf786ef868a744c484699c2465e1f
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-02-03 13:20:30 +0200

    Yampa-Gloss skeleton

---
+++ src/Graphics/Gloss/Interface/FRP/Yampa.hs
@@ -0,0 +1,36 @@
+module Graphics.Gloss.Interface.FRP.Yampa (playYampa, InputEvent) where
+
+import Control.Monad (when)
+import Data.IORef (newIORef, writeIORef, readIORef)
+import Graphics.Gloss (Display, Color, Picture, blank)
+import qualified Graphics.Gloss.Interface.IO.Game as G
+import FRP.Yampa (Event(..), SF, reactInit, react)
+
+-- | A usefule type synonym for Gloss event values, to avoid confusion
+-- between Gloss and Yampa.
+type InputEvent = G.Event
+
+-- | Play the game in a window, updating when the value of the provided 
+playYampa :: Display -- ^ The display method
+          -> Color   -- ^ The background color
+          -> Int     -- ^ The refresh rate, in Hertz
+          -> SF (Event InputEvent) Picture
+          -> IO ()
+playYampa display color frequency mainSF = do
+    picRef <- newIORef blank
+
+    handle <- reactInit
+      (return NoEvent)
+      (\_ updated pic -> when updated (picRef `writeIORef` pic) >> return False)
+      mainSF
+
+    G.playIO display color frequency
+             0
+             (const $ readIORef picRef) -- An action to convert the world to a picture
+             (\e t -> react handle (delta, Just (Event e)) >> return (t + delta)) -- A function to handle input events
+             (\d t -> let delta' = realToFrac d - t
+                     in if delta' > 0
+                          then react handle (delta', Just NoEvent) >> return 0.0
+                          else return (-delta')) -- A function to step the world one iteration. It is passed the period of time (in seconds) needing to be advanced
+  where
+    delta = 0.01 / fromIntegral frequency
--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,16 @@
+{-# LANGUAGE Arrows #-}
+
+import Graphics.Gloss
+import Graphics.Gloss.Interface.FRP.Yampa
+import FRP.Yampa (returnA, Event, SF)
+
+mainSF :: SF (Event InputEvent) Picture
+mainSF = proc e -> do
+  returnA -< blank
+
 main :: IO ()
-main = undefined
+main = playYampa
+         (InWindow "2048 game" (320, 240) (800, 200))
+         white
+         30
+         mainSF
